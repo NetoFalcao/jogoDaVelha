c10e7c6a356c52c907ede178f8ac73963ff58838
NetoFalcao <apoioigneto@gmail.com>
2018-03-10 14:01:53 -0300

    Commitado do Eclipse

---
+++ JogoDavelha/src/com/jogodavelha/Jogo.java
@@ -0,0 +1,62 @@
+package com.jogodavelha;
+
+public class Jogo {
+
+	private Boolean proximaJogadaX;
+	private Boolean[][] tabuleiro = new Boolean[3][3];
+	private boolean iniciou;
+
+	public boolean acabou() {
+		return false;
+	}
+
+	public void setMarcaPrimeiroJogadorX(boolean marcaPrimeiroJogadorX) {
+		if (iniciou) {
+			lancarExcecao();
+		}
+		
+		this.proximaJogadaX = marcaPrimeiroJogadorX;
+		
+		
+	}
+
+	public boolean isMarcaPrimeiroJogadorX(){
+		return proximaJogadaX;
+	}
+
+	public void desenhaMarca(int linha, int coluna) {
+		verificarLimites(linha, coluna);
+		
+		if (tabuleiro[linha][coluna] != null){
+			lancarExcecao();
+		} 
+		if (proximaJogadaX == null){
+			lancarExcecao();
+		}
+	
+		tabuleiro[linha][coluna] = proximaJogadaX;
+		proximaJogadaX = !proximaJogadaX;
+		iniciou = true;
+	}
+
+	private void verificarLimites(int linha, int coluna) {
+		if (coluna < 0 || coluna > 2){
+			lancarExcecao();
+		}
+		
+		if (linha < 0 || linha > 2){
+			lancarExcecao();
+		}
+	}
+
+	private void lancarExcecao() {
+		throw new ExcecaoJogoDaVelha();
+	}
+
+	public Boolean isMarcaXNaPosicao(int linha, int coluna) {
+		verificarLimites(linha, coluna);
+		return tabuleiro[linha][coluna];
+		
+	}
+
+}
---
+++ JogoDavelha/src/com/jogodavelha/JogoTest.java
@@ -0,0 +1,99 @@
+package com.jogodavelha;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JogoTest {
+
+	private Jogo jogo;
+	
+	@Before
+	public void novoJogo() {
+		jogo = new Jogo();
+	}
+	
+	@Test
+	public void criarJogo() {
+		assertFalse("O jogo iniciou finalizado",jogo.acabou());
+		
+	}
+	
+	@Test
+	public void definirPrimeiroJogador() {
+		jogo.setMarcaPrimeiroJogadorX(true);
+		assertTrue(jogo.isMarcaPrimeiroJogadorX());
+	}
+	
+	@Test
+	public void definirPrimeiroJogadorDenovo() {
+		jogo.setMarcaPrimeiroJogadorX(true); //X
+		jogo.setMarcaPrimeiroJogadorX(false); //O
+		assertFalse(jogo.isMarcaPrimeiroJogadorX()); //O
+	}
+	
+	@Test
+	public void desenharPrimeiraMarca() {
+		jogo.setMarcaPrimeiroJogadorX(true);
+		jogo.desenhaMarca(1,0);
+		assertTrue(jogo.isMarcaXNaPosicao(1,0));
+	}
+	
+	@Test(expected=ExcecaoJogoDaVelha.class)
+	public void desenharEmcCelulaOcupada() {
+		jogo.setMarcaPrimeiroJogadorX(true);
+		jogo.desenhaMarca(1,0);
+		jogo.desenhaMarca(1,0);
+	}
+	
+	@Test(expected=ExcecaoJogoDaVelha.class)
+	public void desenharEmcColunaErrada() {
+		jogo.setMarcaPrimeiroJogadorX(false); //O
+		jogo.desenhaMarca(1, 4);
+	}
+	
+	@Test(expected=ExcecaoJogoDaVelha.class)
+	public void desenharEmLinhaErrada() {
+		jogo.setMarcaPrimeiroJogadorX(false); //O
+		jogo.desenhaMarca(-1, 0);
+	}
+	
+	@Test
+	public void lerDeUmaCelulaDesocupada() {
+		jogo.setMarcaPrimeiroJogadorX(false); //O
+		assertNull(jogo.isMarcaXNaPosicao(0, 0));
+	}
+
+	@Test (expected=ExcecaoJogoDaVelha.class)
+	public void lerMarcaDeUmaColunaErrada() {
+		jogo.setMarcaPrimeiroJogadorX(true); //O
+		jogo.isMarcaXNaPosicao(1, 3);
+	}
+	
+	@Test (expected=ExcecaoJogoDaVelha.class)
+	public void lerMarcaDeUmaLinhaErrada() {
+		jogo.setMarcaPrimeiroJogadorX(true); //O
+		jogo.isMarcaXNaPosicao(-1, 1);
+	}
+	
+	@Test (expected=ExcecaoJogoDaVelha.class)
+	public void definirPrimeiroJogadorAposInicioDoJogo() {
+		jogo.setMarcaPrimeiroJogadorX(true); //O
+		jogo.desenhaMarca(1, 0); //inicio Do JOgo
+		jogo.setMarcaPrimeiroJogadorX(true);
+	}
+	
+	@Test (expected=ExcecaoJogoDaVelha.class)
+	public void desenharMarcaAntesDeDefinirPrimeiroJogadorAposInicioDoJogo() {
+		jogo.desenhaMarca(1, 0);
+	}
+	
+	@Test
+	public void desenharSegundaMarca() {
+		jogo.setMarcaPrimeiroJogadorX(true);
+		jogo.desenhaMarca(1,0); // x
+		jogo.desenhaMarca(1, 1); // o
+		assertFalse(jogo.isMarcaXNaPosicao(1, 1));
+	}
+}
